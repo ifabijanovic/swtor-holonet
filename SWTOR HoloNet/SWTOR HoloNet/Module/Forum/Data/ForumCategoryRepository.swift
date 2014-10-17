8f7434511e283233ff1a3a7ce4d2e1c37f8de3ec
Ivan Fabijanovic <feb@shoutem.com>
2014-10-17 15:23:29 +0200

    Implemented ForumCategoryRepository logic.

--- SWTOR HoloNet/SWTOR HoloNet/Module/Forum/Data/ForumCategoryRepository.swift
+++ SWTOR HoloNet/SWTOR HoloNet/Module/Forum/Data/ForumCategoryRepository.swift
@@ -22,18 +22,109 @@ class ForumCategoryRepository {
     
     // MARK: - Public methods
     
-    func get(#language: ForumLanguage, success: ((Array<ForumCategory>) -> Void), error: ((NSError) -> Void)) {
-        
+    func get(#language: ForumLanguage, success: ((Array<ForumCategory>) -> Void), failure: ((NSError) -> Void)) {
+        self.get(id: language.toRaw(), success: success, failure: failure)
     }
     
-    func get(category: ForumCategory, success: ((Array<ForumCategory>) -> Void), error: ((NSError) -> Void)) {
-        
+    func get(#category: ForumCategory, success: ((Array<ForumCategory>) -> Void), failure: ((NSError) -> Void)) {
+        self.get(id: category.id, success: success, failure: failure)
     }
     
-    // MARK: - Private methods
+    // MARK: - Network
+    
+    private func get(#id: Int, success: ((Array<ForumCategory>) -> Void), failure: ((NSError) -> Void)) {
+        let manager = AFHTTPRequestOperationManager()
+        manager.responseSerializer = AFHTTPResponseSerializer()
+        
+        let url = "\(self.rootUrl)?f=\(id)"
+        manager.GET(url, parameters: nil, success: { (operation, response) in
+            let html = operation.responseString
+            let items = self.parseHtml(html)
+            success(items)
+        }) { (operation, error) in
+            failure(error)
+        }
+    }
+    
+    // MARK: - Parsing
     
     private func parseHtml(html: String) -> Array<ForumCategory> {
-        return Array<ForumCategory>()
+        var items = Array<ForumCategory>()
+        
+        let document = HTMLDocument(string: html)
+        let categoryNodes = document.nodesMatchingSelector(".forumCategory > .subForum") as Array<HTMLElement>
+        
+        for node in categoryNodes {
+            let category = self.parseCategory(node)
+            if category != nil {
+                items.append(category!)
+            }
+        }
+        
+        return items
+    }
+    
+    private func parseCategory(element: HTMLElement) -> ForumCategory? {
+        
+        let titleElement = element.firstNodeMatchingSelector(".resultTitle > a")
+        if titleElement == nil {
+            // No title element, skip
+            return nil
+        }
+        
+        // Id
+        let link = titleElement.objectForKeyedSubscript("href") as? String
+        if link == nil {
+            // Link href missing, skip
+            return nil
+        }
+        let id = NSURLComponents(string: link!).queryValueForName("f")
+        if id == nil || id!.toInt() == nil {
+            // Id cannot be parsed, skip
+            return nil
+        }
+        
+        // Title
+        let title = titleElement.textContent
+        
+        let category = ForumCategory(id: id!.toInt()!, title: title)
+        
+        // Icon
+        let thumbElement = element.firstNodeMatchingSelector(".thumbBackground")
+        if thumbElement != nil {
+            let iconStyle = thumbElement.objectForKeyedSubscript("style") as? String
+            if iconStyle != nil {
+                let start = iconStyle!.rangeOfString("url(", options: NSStringCompareOptions.LiteralSearch, range: nil, locale: nil)
+                let end = iconStyle!.rangeOfString(")", options: NSStringCompareOptions.LiteralSearch, range: nil, locale: nil)
+                let range = Range<String.Index>(start: start!.endIndex, end: end!.startIndex)
+                category.iconUrl = iconStyle!.substringWithRange(range)
+            }
+        }
+        
+        // Description
+        let textElement = element.firstNodeMatchingSelector(".resultText")
+        if textElement != nil {
+            let description = textElement.textContent
+            category.description = description
+        }
+        
+        let subTextElements = element.nodesMatchingSelector(".resultSubText") as Array<HTMLElement>
+        
+        // Stats
+        if subTextElements.count > 0 {
+            let statsElement = subTextElements[0]
+            let stats = statsElement.textContent
+            category.stats = stats.stripNewLinesAndTabs()
+        }
+        
+        // Last post
+        if subTextElements.count > 1 {
+            let lastPostElement = subTextElements[1]
+            let lastPost = lastPostElement.textContent
+            category.lastPost = lastPost.stripNewLinesAndTabs()
+        }
+        
+        return category
     }
     
 }
