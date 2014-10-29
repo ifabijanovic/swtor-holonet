abd6a40c70b968bacbd6251e7f08e2525f76f7b3
Ivan Fabijanovic <feb@shoutem.com>
2014-10-29 21:10:08 +0100

    Moved forum thread cell from storyboard to separate nib.

--- SWTOR HoloNet/SWTOR HoloNet/Module/Forum/UI/ForumListTableViewController.swift
+++ SWTOR HoloNet/SWTOR HoloNet/Module/Forum/UI/ForumListTableViewController.swift
@@ -55,6 +55,9 @@ class ForumListTableViewController: ForumBaseTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let bundle = NSBundle.mainBundle()
+        self.tableView.registerNib(UINib(nibName: "ForumThreadTableViewCell", bundle: bundle), forCellReuseIdentifier: ThreadCellIdentifier)
+        
         self.onRefresh()
     }
 
@@ -108,8 +111,9 @@ class ForumListTableViewController: ForumBaseTableViewController {
             cell = tableView.dequeueReusableCellWithIdentifier(CategoryCellIdentifier, forIndexPath: indexPath) as UITableViewCell
             self.setupCategoryCell(cell, indexPath: indexPath)
         } else if indexPath.section == ThreadSection {
-            cell = tableView.dequeueReusableCellWithIdentifier(ThreadCellIdentifier, forIndexPath: indexPath) as UITableViewCell
-            self.setupThreadCell(cell, indexPath: indexPath)
+            let threadCell = tableView.dequeueReusableCellWithIdentifier(ThreadCellIdentifier, forIndexPath: indexPath) as ForumThreadTableViewCell
+            self.setupThreadCell(threadCell, indexPath: indexPath)
+            cell = threadCell
         } else {
             // Safeguard, should not happen
             cell = UITableViewCell()
@@ -263,33 +267,28 @@ class ForumListTableViewController: ForumBaseTableViewController {
         cell.tag = indexPath.row
     }
     
-    private func setupThreadCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+    private func setupThreadCell(cell: ForumThreadTableViewCell, indexPath: NSIndexPath) {
         let thread = self.threads![indexPath.row]
-        let titleLabel = cell.viewWithTag(100) as UILabel
-        let authorLabel = cell.viewWithTag(101) as UILabel
-        let devImageView = cell.viewWithTag(102) as UIImageView
-        let stickyImageView = cell.viewWithTag(103) as UIImageView
-        let repliesViewsLabel = cell.viewWithTag(104) as UILabel
         
         // Set dev icon if thread is marked as having Bioware reply
         if thread.hasBiowareReply {
-            devImageView.hidden = false
-            devImageView.sd_setImageWithURL(NSURL(string: self.settings!.devTrackerIconUrl), placeholderImage: UIImage(named: "DevTrackerIcon"))
+            cell.devImageView.hidden = false
+            cell.devImageView.sd_setImageWithURL(NSURL(string: self.settings!.devTrackerIconUrl), placeholderImage: UIImage(named: "DevTrackerIcon"))
         } else {
-            devImageView.hidden = true
+            cell.devImageView.hidden = true
         }
         
         // Set sticky icon if thread is marked with sticky
         if thread.isSticky {
-            stickyImageView.hidden = false
-            stickyImageView.sd_setImageWithURL(NSURL(string: self.settings!.stickyIconUrl), placeholderImage: UIImage(named: "StickyIcon"))
+            cell.stickyImageView.hidden = false
+            cell.stickyImageView.sd_setImageWithURL(NSURL(string: self.settings!.stickyIconUrl), placeholderImage: UIImage(named: "StickyIcon"))
         } else {
-            stickyImageView.hidden = true
+            cell.stickyImageView.hidden = true
         }
         
-        titleLabel.text = thread.title
-        authorLabel.text = thread.author
-        repliesViewsLabel.text = "R: \(thread.replies), V: \(thread.views)"
+        cell.titleLabel.text = thread.title
+        cell.authorLabel.text = thread.author
+        cell.repliesViewsLabel.text = "R: \(thread.replies), V: \(thread.views)"
         
         cell.tag = indexPath.row
     }
---
+++ SWTOR HoloNet/SWTOR HoloNet/Module/Forum/UI/TableCell/ForumThreadTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ForumThreadTableViewCell.swift
+//  SWTOR HoloNet
+//
+//  Created by Ivan Fabijanovic on 29/10/14.
+//  Copyright (c) 2014 Ivan Fabijanović. All rights reserved.
+//
+
+import UIKit
+
+class ForumThreadTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var devImageView: UIImageView!
+    @IBOutlet weak var stickyImageView: UIImageView!
+    @IBOutlet weak var repliesViewsLabel: UILabel!
+    
+}
