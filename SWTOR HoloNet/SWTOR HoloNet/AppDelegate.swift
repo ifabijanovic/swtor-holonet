70b7cb463864f40331ee2bdae573808685deb089
Ivan Fabijanovic <feb@shoutem.com>
2015-05-06 19:18:24 +0200

    Added missing Bolts import.

--- SWTOR HoloNet/SWTOR HoloNet/AppDelegate.swift
+++ SWTOR HoloNet/SWTOR HoloNet/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Parse
 import ParseCrashReporting
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- SWTOR HoloNet/SWTOR HoloNet/Module/Push/PushManager.swift
+++ SWTOR HoloNet/SWTOR HoloNet/Module/Push/PushManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Parse
+import Bolts
 
 let keyDidCancelPushAccess = "pushDidCancelPushAccess"
 let keyDidApprovePushAccess = "pushDidApprovePushAccess"
