3459972ae0bef2e21a7313591be08ae7b239a086
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2017-02-18 20:01:23 +0100

    Travis CI setup

---
+++ scripts/bootstrap-if-needed.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
+  echo "Dependencies out of date, bootstrapping..."
+  scripts/bootstrap.sh
+else
+  echo "Dependencies up to date"
+fi
+
