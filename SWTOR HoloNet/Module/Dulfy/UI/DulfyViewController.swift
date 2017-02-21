b7678e1f2318c5ef434c409d315d440fd1688162
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2017-02-21 09:08:10 +0100

    Removed deprecated UIWebView support in DulfyViewController

--- SWTOR HoloNet/Module/Dulfy/UI/DulfyViewController.swift
+++ SWTOR HoloNet/Module/Dulfy/UI/DulfyViewController.swift
@@ -9,69 +9,35 @@
 import UIKit
 import WebKit
 
-// Workaround solution implemented because of a bug with protocol extension in Swift 1.2 and iOS SDK 8.3
-// See WebViewEx.swift file for more info
-
-class DulfyViewController: BaseViewController, ActionPerformer, UIWebViewDelegate, WKNavigationDelegate {
+class DulfyViewController: BaseViewController {
+    fileprivate var wkWebView: WKWebView!
     
-    // MARK: - Properties
+    fileprivate var homeUrl: URL { return URL(string: self.settings.dulfyNetUrl)! }
+    fileprivate var url: URL?
     
-    let useWebKit = objc_getClass("WKWebView") != nil
-    
-    var wkWebView: WKWebView?
-    var uiWebView: UIWebView?
-    
-    var homeUrl: URL {
-        get {
-            return URL(string: self.settings.dulfyNetUrl)!
-        }
-    }
-    var url: URL?
-    
-    var isVisible = false
-    
-    // MARK: - Outlets
+    fileprivate var isVisible = false
     
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     @IBOutlet var backButton: UIBarButtonItem!
     @IBOutlet var forwardButton: UIBarButtonItem!
     
     @IBAction func backTapped(_ sender: AnyObject) {
-        if self.useWebKit {
-            self.wkWebView!.goBack()
-        } else {
-            self.uiWebView!.goBack()
-        }
+        self.wkWebView.goBack()
     }
     
     @IBAction func forwardTapped(_ sender: AnyObject) {
-        if self.useWebKit {
-            self.wkWebView!.goForward()
-        } else {
-            self.uiWebView!.goForward()
-        }
+        self.wkWebView.goForward()
     }
     
     @IBAction func reloadTapped(_ sender: AnyObject) {
-        if self.useWebKit {
-            self.wkWebView!.reload()
-        } else {
-            self.uiWebView!.reload()
-        }
+        self.wkWebView.reload()
     }
     
     @IBAction func stopTapped(_ sender: AnyObject) {
-        if self.useWebKit {
-            self.wkWebView!.stopLoading()
-            self.navigationItem.title = self.wkWebView!.title
-            self.backButton.isEnabled = self.wkWebView!.canGoBack
-            self.forwardButton.isEnabled = self.wkWebView!.canGoForward
-        } else {
-            self.uiWebView!.stopLoading()
-            self.navigationItem.title = self.uiWebView!.stringByEvaluatingJavaScript(from: "document.title")
-            self.backButton.isEnabled = self.uiWebView!.canGoBack
-            self.forwardButton.isEnabled = self.uiWebView!.canGoForward
-        }
+        self.wkWebView.stopLoading()
+        self.navigationItem.title = self.wkWebView.title
+        self.backButton.isEnabled = self.wkWebView.canGoBack
+        self.forwardButton.isEnabled = self.wkWebView.canGoForward
         
         self.activityIndicator.stopAnimating()
     }
@@ -81,18 +47,12 @@ class DulfyViewController: BaseViewController, ActionPerformer, UIWebViewDelegat
     }
     
     @IBAction func safariTapped(_ sender: AnyObject) {
-        if self.useWebKit {
-            if let url = self.wkWebView!.url {
-                UIApplication.shared.openURL(url)
-            }
-        } else {
-            if let url = self.uiWebView!.request?.url {
-                UIApplication.shared.openURL(url)
-            }
+        if let url = self.wkWebView.url {
+            UIApplication.shared.openURL(url)
         }
     }
 
-    // MARK: - Lifecycle
+    // MARK: - Overrides
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -111,12 +71,10 @@ class DulfyViewController: BaseViewController, ActionPerformer, UIWebViewDelegat
         self.url = nil
         
         // Enable hide actions on navigation controller if they are available
-        if self.useWebKit {
-            if let navController = self.navigationController {
-                navController.hidesBarsOnSwipe = true
-                navController.hidesBarsWhenVerticallyCompact = true
-                navController.hidesBarsWhenKeyboardAppears = true
-            }
+        if let navController = self.navigationController {
+            navController.hidesBarsOnSwipe = true
+            navController.hidesBarsWhenVerticallyCompact = true
+            navController.hidesBarsWhenKeyboardAppears = true
         }
         
         #if !DEBUG && !TEST
@@ -126,11 +84,8 @@ class DulfyViewController: BaseViewController, ActionPerformer, UIWebViewDelegat
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if self.useWebKit {
-            self.wkWebView!.navigationDelegate = self
-        } else {
-            self.uiWebView!.delegate = self
-        }
+        
+        self.wkWebView.navigationDelegate = self
         self.isVisible = true
         
         // Navigate to custom url if set
@@ -142,43 +97,10 @@ class DulfyViewController: BaseViewController, ActionPerformer, UIWebViewDelegat
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        if self.useWebKit {
-            self.wkWebView!.navigationDelegate = nil
-        } else {
-            self.uiWebView!.delegate = nil
-        }
+        
+        self.wkWebView.navigationDelegate = nil
         self.isVisible = false
     }
-    
-    // MARK: - UIWebViewDelegate
-    
-    func webViewDidStartLoad(_ webView: UIWebView) {
-        self.activityIndicator.startAnimating()
-    }
-    
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        self.activityIndicator.stopAnimating()
-        
-        self.navigationItem.title = webView.stringByEvaluatingJavaScript(from: "document.title")
-        self.backButton.isEnabled = webView.canGoBack
-        self.forwardButton.isEnabled = webView.canGoForward
-    }
-    
-    // MARK: - WKNavigationDelegate
-    
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        self.activityIndicator.startAnimating()
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        self.activityIndicator.stopAnimating()
-        
-        self.navigationItem.title = webView.title
-        self.backButton.isEnabled = webView.canGoBack
-        self.forwardButton.isEnabled = webView.canGoForward
-    }
-    
-    // MARK: - Themeable
     
     override func applyTheme(_ theme: Theme) {
         super.applyTheme(theme)
@@ -193,9 +115,49 @@ class DulfyViewController: BaseViewController, ActionPerformer, UIWebViewDelegat
         let animate = self.isViewLoaded && self.view.window != nil
         theme.apply(toolbar: self.navigationController!.toolbar, animate: animate)
     }
+}
+
+extension DulfyViewController {
+    func navigateTo(_ url: URL) {
+        let request = URLRequest(url: url)
+        self.wkWebView.load(request)
+    }
     
-    // MARK: - ActionPerformer
+    fileprivate func setupWebView() {
+        self.wkWebView = WKWebView()
+        // Insert the webView at index 0 so its scroll view insets get adjusted automatically
+        self.view.insertSubview(self.wkWebView, at: 0)
+        // Disable autoresizing mask translation so auto layout constraints can be added
+        self.wkWebView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Define constraints that make the webView display in full screen
+        let views: [String: Any] = ["webView": self.wkWebView]
+        var constraints = [NSLayoutConstraint]()
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|[webView]|", options: [], metrics: nil, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|[webView]|", options: [], metrics: nil, views: views)
+        NSLayoutConstraint.activate(constraints)
+        
+        // Make the webView transparent
+        self.wkWebView.isOpaque = false
+        self.wkWebView.backgroundColor = UIColor.clear
+    }
+}
+
+extension DulfyViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        self.activityIndicator.startAnimating()
+    }
     
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.activityIndicator.stopAnimating()
+        
+        self.navigationItem.title = webView.title
+        self.backButton.isEnabled = webView.canGoBack
+        self.forwardButton.isEnabled = webView.canGoForward
+    }
+}
+
+extension DulfyViewController: ActionPerformer {
     func perform(userInfo: [AnyHashable : Any]) {
         if let url = userInfo["url"] as? URL {
             if self.isVisible {
@@ -205,47 +167,4 @@ class DulfyViewController: BaseViewController, ActionPerformer, UIWebViewDelegat
             }
         }
     }
-    
-    // MARK: - Public methods
-    
-    func navigateTo(_ url: URL) {
-        let request = URLRequest(url: url)
-        if self.useWebKit {
-            self.wkWebView!.load(request)
-        } else {
-            self.uiWebView!.loadRequest(request)
-        }
-    }
-    
-    // MARK: - Private methods
-    
-    internal func setupWebView() {
-        // Use WKWebView if available, fallback to UIWebView
-        let webView: UIView
-        if self.useWebKit {
-            self.wkWebView = WKWebView()
-            webView = self.wkWebView!
-        } else {
-            self.uiWebView = UIWebView()
-            webView = self.uiWebView!
-        }
-        // Insert the webView at index 0 so its scroll view insets get adjusted automatically
-        self.view.insertSubview(webView, at: 0)
-        // Disable autoresizing mask translation so auto layout constraints can be added
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        
-        // Define constraints that make the webView display in full screen
-        let trailing = NSLayoutConstraint(item: self.view, attribute: .trailing, relatedBy: .equal, toItem: webView, attribute: .trailing, multiplier: 1.0, constant: 0)
-        let leading = NSLayoutConstraint(item: self.view, attribute: .leading, relatedBy: .equal, toItem: webView, attribute: .leading, multiplier: 1.0, constant: 0)
-        let bottom = NSLayoutConstraint(item: self.view, attribute: .bottom, relatedBy: .equal, toItem: webView, attribute: .bottom, multiplier: 1.0, constant: 0)
-        let top = NSLayoutConstraint(item: self.view, attribute: .top, relatedBy: .equal, toItem: webView, attribute: .top, multiplier: 1.0, constant: 0)
-        
-        // Apply the auto layout constraints
-        self.view.addConstraints([trailing, leading, bottom, top])
-        
-        // Make the webView transparent
-        webView.isOpaque = false
-        webView.backgroundColor = UIColor.clear
-    }
-
 }
