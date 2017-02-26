491aaea3c85786b6ab148d7e2e2cbecad51c592e
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2017-02-26 09:59:46 +0100

    Implemented navigation methods in Navigator

--- SWTOR HoloNet/Module/App/UI/Navigator.swift
+++ SWTOR HoloNet/Module/App/UI/Navigator.swift
@@ -11,6 +11,12 @@ import UIKit
 typealias AlertActionHandler = (UIAlertAction) -> Void
 
 enum NavigationState {
+    case forumCategory(item: ForumCategory)
+    case forumThread(item: ForumThread)
+    case forumPost(item: ForumPost)
+    case themeSettings
+    case textSizeSettings
+    case text(title: String, path: String)
 }
 
 protocol Navigator {
@@ -19,6 +25,7 @@ protocol Navigator {
     func showMaintenanceAlert(handler: AlertActionHandler?)
     
     func navigate(from: UIViewController, to: NavigationState, animated: Bool)
+    func open(url: URL)
 }
 
 struct DefaultNavigator: Navigator {}
@@ -56,6 +63,89 @@ extension DefaultNavigator {
 
 extension DefaultNavigator {
     func navigate(from: UIViewController, to: NavigationState, animated: Bool) {
+        switch to {
+        case .forumCategory(let item):
+            self.navigate(from: from, to: item, animated: animated)
+            break
+        case .forumThread(let item):
+            self.navigate(from: from, to: item, animated: animated)
+            break
+        case .forumPost(let item):
+            self.navigate(from: from, to: item, animated: animated)
+            break
+        case .themeSettings:
+            self.navigateToThemeSettings(from: from, animated: animated)
+            break
+        case .textSizeSettings:
+            self.navigateToTextSizeSettings(from: from, animated: animated)
+            break
+        case .text(let title, let path):
+            self.navigate(from: from, textTitle: title, textPath: path, animated: animated)
+            break
+        }
+    }
+    
+    func open(url: URL) {
+        UIApplication.shared.openURL(url)
+    }
+    
+    private func navigate(from: UIViewController, to forumCategory: ForumCategory, animated: Bool) {
+        guard from is ForumListCollectionViewController else {
+            assert(false, "Navigating to forumCategory from invalid predecessor")
+            return
+        }
         
+        let categoryRepository = DefaultForumCategoryRepository(settings: InstanceHolder.sharedInstance.settings)
+        let threadRepository = DefaultForumThreadRepository(settings: InstanceHolder.sharedInstance.settings)
+        let successor = ForumListCollectionViewController(category: forumCategory, categoryRepository: categoryRepository, threadRepository: threadRepository)
+        from.navigationController?.pushViewController(successor, animated: animated)
+    }
+    
+    private func navigate(from: UIViewController, to forumThread: ForumThread, animated: Bool) {
+        guard from is ForumListCollectionViewController else {
+            assert(false, "Navigating to forumThread from invalid predecessor")
+            return
+        }
+        
+        let postRepository = DefaultForumPostRepository(settings: InstanceHolder.sharedInstance.settings)
+        let successor = ForumThreadCollectionViewController(thread: forumThread, postRepository: postRepository)
+        from.navigationController?.pushViewController(successor, animated: animated)
+    }
+    
+    private func navigate(from: UIViewController, to forumPost: ForumPost, animated: Bool) {
+        guard from is ForumThreadCollectionViewController else {
+            assert(false, "Navigating to forumPost from invalid predecessor")
+            return
+        }
+        
+        let successor = ForumPostViewController(post: forumPost)
+        from.navigationController?.pushViewController(successor, animated: animated)
+    }
+    
+    private func navigateToThemeSettings(from: UIViewController, animated: Bool) {
+        guard from is SettingsTableViewController else {
+            assert(false, "Navigating to themeSettings from invalid predecessor")
+            return
+        }
+        
+        let successor = ThemeSettingsTableViewController()
+        from.navigationController?.pushViewController(successor, animated: animated)
+    }
+    
+    private func navigateToTextSizeSettings(from: UIViewController, animated: Bool) {
+        guard from is SettingsTableViewController else {
+            assert(false, "Navigating to textSizeSettings from invalid predecessor")
+            return
+        }
+        
+        let successor = TextSizeSettingsTableViewController()
+        from.navigationController?.pushViewController(successor, animated: animated)
+    }
+    
+    private func navigate(from: UIViewController, textTitle: String, textPath: String, animated: Bool) {
+        let successor = TextViewController()
+        successor.title = textTitle
+        successor.file = textPath
+        from.navigationController?.pushViewController(successor, animated: animated)
     }
 }
