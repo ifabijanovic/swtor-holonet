350172b52716dfc9611988084d04acfcfe3e6fd7
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2017-03-12 20:32:31 +0100

    Renamed project and folders to just HoloNet

--- ios/scripts/resources.sh
+++ ios/scripts/resources.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -f "${PROJECT_DIR}/SWTOR HoloNet/GoogleService-Info.plist" ]; then 
-  cp "${PROJECT_DIR}/SWTOR HoloNet/GoogleService-Info.plist" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/GoogleService-Info.plist"
+if [ -f "${PROJECT_DIR}/HoloNet/GoogleService-Info.plist" ]; then 
+  cp "${PROJECT_DIR}/HoloNet/GoogleService-Info.plist" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/GoogleService-Info.plist"
 fi
 
