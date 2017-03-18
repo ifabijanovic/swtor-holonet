5b4773c80af86e709968dd79aefe2b7d087251c7
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2017-03-18 12:01:05 +0100

    Added an empty Android project with Kotlin configured
    - configure travis to build both Android and iOS apps

---
+++ android/app/src/main/java/com/ifabijanovic/holonet/RootActivity.kt
@@ -0,0 +1,11 @@
+package com.ifabijanovic.holonet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}
